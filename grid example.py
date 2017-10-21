224fbea215809090e338a871addb47fb9361cefd
neil-oliver <neiloliverphoto@gmail.com>
2017-10-21 22:44:24 +0100

    introducing pygame
    just coloured blocks so far...

--- grid example.py
+++ grid example.py
@@ -1,19 +1,34 @@
 #20x20 grid
+
+import pygame
 import random
+import time
 
 grid = []
 tracker = []
+pattern = []
 
 def printGrid(grid):
     #pretty printing of grid
     for row in grid:
         print(row)
     print("\n")
-        
 
-def create(size):
+def drawGrid(grid):
+    # Draw the grid
+    for row in range(len(grid)):
+        for column in range(len(grid)):
+            color = WHITE
+            if grid[row][column] == 1:
+                color = BLACK
+            pygame.draw.rect(screen, color,
+                             [(WIDTH) * column, (HEIGHT) * row, WIDTH, HEIGHT])
+
+
+def createGrid(size):
     #create grid of a given size with random 1's & 0's
     global grid
+    grid = []
     for i in range(size):
         row = []
         for x in range(size):
@@ -55,7 +70,7 @@ def stripTracker(pattern):
 def createPattern(patternNumber):
     #create a grid with a set pattern
     global grid
-    pattern = []
+    global pattern
     value = 1
 
     if patternNumber == 0:
@@ -117,26 +132,25 @@ def createPattern(patternNumber):
                 for x in range(0, len(grid)):
                     row.append(value)
                 value = 1
-
-
             pattern.append(row)
         return pattern
 
 def nextPattern(patternNumber):
-    for repeat in range(1,4):
-        print("Running Next Pattern")
-        if patternNumber == 4:
-            patternNumber = 0
-            randomReplace(patternNumber)
-            patternNumber += 1
-        else:
-            patternNumber += 1
-            randomReplace(patternNumber)
+    global pattern
+    patternNumber += 1
+    if patternNumber == 5:
+        patternNumber = 0
+
+    pattern = []
+    pattern = createPattern(patternNumber)
+    stripTracker(pattern)
+    return patternNumber
 
 def randomReplace(patternNumber):
     #randomly replace elements until a pattern has been achieved
     global grid
     global tracker
+    global pattern
 
     createTracker()
     pattern = createPattern(patternNumber)
@@ -150,6 +164,7 @@ def randomReplace(patternNumber):
 
         contents = tracker[selectedRow].pop(selectedCell)
         grid[contents[0]][contents[1]] = pattern[contents[0]][contents[1]]
+
         printGrid(grid)
 
         if len(tracker[selectedRow]) == 0:
@@ -157,24 +172,81 @@ def randomReplace(patternNumber):
 
     print("*** Pattern Achieved *** \n")
 
+
+
 #-----------------------------------------------------------#
 
 size = int(input("please enter a size between 5 & 50: \n"))
 if size >= 5 and size <= 50:
-    create(size)
-    pattern = int(input("please select a pattern from the following list: \n 0. All 00000's \n 1. All 11111's \n 2. Alternating 101010 \n 3. Alternating 101010 same each line \n 4. Alternating lines of 1's and 0's \n"))
+    createGrid(size)
+    patternSelection = int(input("please select a pattern from the following list: \n 0. All 00000's \n 1. All 11111's \n 2. Alternating 101010 \n 3. Alternating 101010 same each line \n 4. Alternating lines of 1's and 0's \n"))
 
-    if pattern >= 0 and pattern <= 4:
+    if patternSelection >= 0 and patternSelection <= 4:
         print("Starting with a " + str(size) + " X " + str(size) + " grid.")
         print("Modifying values to achieve pattern... \n")
-        randomReplace(pattern)
+        #randomReplace(patternSelection)
+        #nextPattern(patternSelection)
     else:
         print("incorrect menu selection entered")
 else:
     print("incorrect size entered")
 
-nextPattern(pattern + 1)
+
+#-----------------------------------------------------------#
+
+# Define some colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
 
 
+# Initialize pygame
+pygame.init()
 
+# Set the HEIGHT and WIDTH of the screen
+WINDOW_SIZE = [600, 600]
+screen = pygame.display.set_mode(WINDOW_SIZE)
 
+WIDTH = WINDOW_SIZE[0] / len(grid)
+HEIGHT = WINDOW_SIZE[0] / len(grid)
+
+# Set title of screen
+pygame.display.set_caption("Grid Pattern")
+
+# Loop until the user clicks the close button.
+done = False
+
+# Used to manage how fast the screen updates
+clock = pygame.time.Clock()
+
+createTracker()
+pattern = createPattern(patternSelection)
+stripTracker(pattern)
+printGrid(grid)
+
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+    if tracker != []:
+
+        selectedRow = random.randint(0, len(tracker) - 1)
+        selectedCell = random.randint(0,len(tracker[selectedRow]) - 1)
+
+        contents = tracker[selectedRow].pop(selectedCell)
+        grid[contents[0]][contents[1]] = pattern[contents[0]][contents[1]]
+        drawGrid(grid)
+
+        if len(tracker[selectedRow]) == 0:
+            tracker.pop(selectedRow)
+    else:
+        createTracker()
+        patternSelection = nextPattern(patternSelection)
+
+    pygame.display.flip()
+
+    clock.tick(15)
+
+pygame.quit()
